18b7b8654be1bf36df4f126fc1530d8d4475e0e9
Joel McCracken <jmccracken@thinkthroughmath.com>
2015-02-04 10:11:57 -0500

    org plugin

--- _lib/org-convert.el
+++ _lib/org-convert.el
@@ -1,19 +1,58 @@
-
 (require 'org)
 (require 'ox)
 (require 'ox-html)
 
+;; only supports a single client at a time.
 
-(defun compile-org-file ()
+(defun compile-org-file (content)
   (interactive)
-  (let ((org-document-content "")
-        this-read)
-    (while (setq this-read (ignore-errors
-                             (read-from-minibuffer "")))
-      (setq org-document-content (concat org-document-content "\n" this-read)))
-
     (with-temp-buffer
       (org-mode)
-      (insert org-document-content)
+      (insert content)
       (org-html-export-as-html nil nil nil t)
-      (princ (buffer-string)))))
+      (buffer-string)))
+
+(defun org-jekyll-daemon-sentinel (process event)
+  ;; not doing anything for now
+  )
+
+
+(defvar org-jekyll-convert-string nil)
+(defvar org-jekyll-convert-string-size nil)
+
+(defun org-jekyll-str-received (process str)
+  (setq org-jekyll-convert-string
+        (concat org-jekyll-convert-string
+                str))
+  (unless org-jekyll-convert-string-size
+    (when (string-match "Length: \\([[:digit:]]+\\)\n" org-jekyll-convert-string)
+      (setq org-jekyll-convert-string-size
+            (string-to-number (match-string 1
+                                            org-jekyll-convert-string)))
+      (setq org-jekyll-convert-string
+            (replace-match "" nil nil
+                           org-jekyll-convert-string))))
+
+  (when org-jekyll-convert-string-size
+    (when (= (string-bytes org-jekyll-convert-string)
+             org-jekyll-convert-string-size)
+
+      (process-send-string process
+                           (compile-org-file org-jekyll-convert-string))
+      (delete-process process)
+      (setq org-jekyll-convert-string nil
+            org-jekyll-convert-string-size nil))))
+
+(defun org-jekyll-daemon-filter (process string)
+  (org-jekyll-str-received process string))
+
+(defvar org-jekyll-server-process nil)
+
+(defun start-compile-server ()
+  (setq org-jekyll-server-process
+        (make-network-process :name "org-jekyll-daemon"
+                              :server t
+                              :sentinel 'org-jekyll-daemon-sentinel
+                              :filter 'org-jekyll-daemon-filter
+                              :service 9876
+                              :host 'local)))
--- _plugins/org-converter.rb
+++ _plugins/org-converter.rb
@@ -12,22 +12,31 @@ module Jekyll
       ".html"
     end
 
+    def ensure_server!
+      @server_pid ||=
+        begin
+          emacs_execution_string = "emacs -Q --daemon=org-convert-daemon -L " \
+            "_vendor/org-8.2.6/lisp/ -l _lib/org-convert.el -f start-compile-server"
+          server_pid = spawn(emacs_execution_string)
+
+          at_exit do
+            spawn("emacsclient -s org-convert-daemon -e '(kill-emacs)'")
+          end
+
+          sleep 3 # give the process a few secs to warm up
+
+          server_pid
+        end
+    end
+
     def convert(content)
-      inread, inwrite = IO.pipe
-      outread, outwrite = IO.pipe
-
-      inwrite.write content
-      inwrite.close
-      emacs_execution_string = "emacs -Q --batch -L " \
-        "_vendor/org-8.2.6/lisp/ -l _lib/org-convert.el -f compile-org-file"
-      org_pid = spawn(emacs_execution_string, :in => inread, :out => outwrite, :err => :out)
-      inread.close
-      outwrite.close
-      Process.wait(org_pid)
-
-      out_content = outread.read
-      outread.close
-      out_content
+      require 'socket'
+      ensure_server!
+      TCPSocket.open 'localhost', 9876 do |socket|
+        socket.puts "Length: #{content.bytesize}"
+        socket.write content
+        socket.read
+      end
     end
   end
 end
