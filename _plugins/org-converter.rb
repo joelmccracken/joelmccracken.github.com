7626321d1303754f43542269f97d3acb450e115a
Joel McCracken <jmccracken@thinkthroughmath.com>
2015-02-15 15:29:02 -0500

    specify emacs path
    I recently installed homebrew emacs on my mac, and that emacs comes
    first in the path -> the wrong emacs was being used.
    
    I'm not sure what the exact problem was, but most of my elisp doesn't
    work with brew emacs (something I intend to debug at some point)

--- _plugins/org-converter.rb
+++ _plugins/org-converter.rb
@@ -15,7 +15,8 @@ module Jekyll
     def ensure_server!
       @server_pid ||=
         begin
-          emacs_execution_string = "emacs -Q --daemon=org-convert-daemon -L " \
+          emacs_execution_string = "/Applications/Emacs.app/Contents/MacOS/Emacs -Q" \
+          " --daemon=org-convert-daemon -L " \
             "_vendor/org-8.2.6/lisp/ -l _lib/org-convert.el -f start-compile-server"
           server_pid = spawn(emacs_execution_string)
 
