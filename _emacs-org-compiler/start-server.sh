cb0b9d2e1f53e4fb803f79f90c5cbe2ef3f88651
Joel McCracken <mccracken.joel@gmail.com>
2017-12-28 17:49:05 -0500

    kill org compiler with sigkill

--- _emacs-org-compiler/start-server.sh
+++ _emacs-org-compiler/start-server.sh
@@ -2,6 +2,6 @@
 
 emacs -Q --daemon -L /org-1.2.6/lisp -l /org-convert.el -f start-compile-server
 while true; do
-    echo "sleeping..."
+    echo sleeping... $(date "+%Y-%m-%d %H:%M:%S")
     sleep 100 || exit 10;
 done
