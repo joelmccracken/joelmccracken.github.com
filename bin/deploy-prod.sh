150d6b5321841b0b0ede31964f5e3c3fa1897bac
Joel McCracken <mccracken.joel@gmail.com>
2017-12-28 19:18:09 -0500

    Site updated at: 2017-12-29 00:18:09 UTC

--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -2,5 +2,6 @@
 set -e
 docker-compose -f ./docker-compose.prod.yml build
 docker-compose -f ./docker-compose.prod.yml down
-docker-compose -f ./docker-compose.prod.yml up -d
+docker-compose -f ./docker-compose.prod.yml up emacs-org-compiler -d
+docker-compose -f ./docker-compose.prod.yml jekyll build
 octopress deploy
