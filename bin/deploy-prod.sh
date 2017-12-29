1150dbd9c3244c834c8e31f0e35c3e423a9d1e1f
Joel McCracken <mccracken.joel@gmail.com>
2017-12-28 19:18:30 -0500

    Site updated at: 2017-12-29 00:18:30 UTC

--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-set -e
 docker-compose -f ./docker-compose.prod.yml build
 docker-compose -f ./docker-compose.prod.yml down
-docker-compose -f ./docker-compose.prod.yml up emacs-org-compiler -d
-docker-compose -f ./docker-compose.prod.yml jekyll build
+# docker-compose -f ./docker-compose.prod.yml -d up emacs-org-compiler
+docker-compose -f ./docker-compose.prod.yml run jekyll-base jekyll build
 octopress deploy
